bf9a044ceddd1067b54b3f201117d39f50974322
Adrian Davila-Zuniga <adriandavila-zuniga@adz-macbook.local>
2013-09-01 14:31:16 -0400

    frame work and paperscript

---
+++ js/paper.js
@@ -0,0 +1,43 @@
+
+
+var ground = {
+
+
+
+
+
+	onFrame : function(e){
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+/****************
+
+	Standard Functions
+
+****************/
+
+function onMouseMove(e){
+
+}
+
+
+function onFrame(e){
+
+	ground.onFrame(e);
+}
+
+
+function onResize(e){
+
+
+}
