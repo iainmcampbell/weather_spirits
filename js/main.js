e06d9a7ad4471b7a88b5ef71abc0f6786b5076b5
iainmcampbell <intrepid4968@gmail.com>
2013-09-01 13:52:08 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+// (function(window){
+
+
+	var api = {
+
+		init : function(){
+			console.log('weather api')
+		}
+
+	}
+
+
+
+
+
+	var app = {
+		init : function(){
+			api.init();
+		}
+	}
+
+	$(document).ready(function(){
+		app.init();
+	})
+
+
+
+// })(window);
