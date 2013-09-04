663450f34e16334fdef6a5fc4bfc86cf337e354d
Adrian Davila-Zuniga <adriandavila-zuniga@adz-macbook.gateway.2wire.net>
2013-09-03 21:45:53 -0400

    Rough hills, clearrect does not work

--- js/main.js
+++ js/main.js
@@ -18,6 +18,54 @@
 
 	var app = {
 		init : function(){
+
+			myCanvas.width = document.body.clientWidth; //document.width is obsolete
+			myCanvas.height = document.body.clientHeight;
+			
+			var c=document.getElementById("myCanvas");
+			var ctx=c.getContext("2d");
+			
+			function randonum(){
+			    	return Math.floor((Math.random()*10)*(Math.random()*100)+100);
+			    }
+			
+			var randomNUM= randonum();
+			
+			var randomHI= randonum();
+			
+			var finalpoint = randomNUM + xwee;
+			
+			var xwee=0;
+			
+			var items = [[xwee,400],[randomNUM,randomHI],[finalpoint,400]];
+			console.log(items[0][1]); // 1
+			
+			console.log(randonum);
+			
+			
+			
+			
+			function onFrame(event) {
+				xwee+=5;
+				randomNUM+=5;
+			
+				if(xwee>myCanvas.width+200) {
+					xwee=0;
+					randomHI= randonum();
+					randomNUM= randonum();
+				}
+			
+				myPath = new Path();
+				myPath.strokeColor = 'black';
+				myPath.add(new Point(-200, 400));
+				myPath.add(new Point(xwee, items[0][1]), new Point( (randomNUM/2) + xwee, randomHI), new Point((randomNUM + xwee), items[2][1]));
+				myPath.add(new Point(myCanvas.width+200, 400));
+			
+			    context.clearRect ( 0 , 0 , myCanvas.width , myCanvas.height );
+			
+				console.log(xwee , randomNUM , randomHI);
+			
+			}
 			api.init();
 		}
 	}
